8e43b3a70f738c87d0da13a525130c7f48c63453
Martin Gauthier <gauthiermartin86@gmail.com>
2020-06-20 15:13:47 -0400

    Commented all console.log()

--- src/commands/diagnose.ts
+++ src/commands/diagnose.ts
@@ -117,7 +117,7 @@ export default class Diagnose extends Command {
             })
 
 
-            console.log("Name:", name);
+            // console.log("Name:", name);
           })
 
         }
