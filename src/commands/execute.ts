aeade7317c29b55dd316575a6e26f0d120fa9c6e
Marc-Antoine Couture <coumarc9@outlook.com>
2020-06-22 16:16:40 -0400

    Adding execute command

--- src/commands/execute.ts
+++ src/commands/execute.ts
@@ -8,12 +8,7 @@ import { execSync } from 'child_process'
 
 const actionExpression = new RegExp("\\{\\{(.*?)\\}\\}", "g");
 
-const replacement = () => {
-  console.log(replacement.caller);
-  return (_: string, group1: string) => eval(group1);
-};
-
-const FilteredPlatforms = Config.filter(x => x.devices.find(y => y.execute));
+const filteredPlatforms = Config.filter(x => x.devices.find(y => y.execute));
 
 export default class Execute extends Command {
   static description = 'Execute a command to a remote device'
@@ -33,13 +28,13 @@ export default class Execute extends Command {
   static args = [
     {
       name: 'platform',
-      options: FilteredPlatforms.map(x => x.name),
+      options: filteredPlatforms.map(x => x.name),
       required: true,
       description: "Platform to target"
     },
     {
       name: 'cmd',
-      options: [...new Set(FilteredPlatforms.map(x => x.devices).flat(1).filter(x => x.execute).map(x => x.execute!).flat(1).map(x => x.name))],
+      options: [...new Set(filteredPlatforms.map(x => x.devices).flat(1).filter(x => x.execute).map(x => x.execute!).flat(1).map(x => x.name))],
       required: true,
       description: "Command to execute (must be contain in specified platform-device)"
     }
@@ -52,37 +47,38 @@ export default class Execute extends Command {
    * @returns deviceName  Specific device or undefined for all devices
    */
   parseArgs(args: { [name: string]: any; }) {
-    const platforms: IPlatform[] = [];
-    let deviceName: string | undefined;
-    if (args.platform) {
-      const platform = FilteredPlatforms.find(x => x.name == args.platform)!;
-      platforms.push(platform)
 
-      if (args.device) {
-        const device = platform.devices.find(x => x.name === args.device && x.diagnose);
+    const { platform: platformName, cmd: cmdName } = args;
 
-        if (!device) {
-          throw "device is not valid for this platform";
-        }
+    const platform = filteredPlatforms.find(x => x.name === platformName)!;
+    const device = platform.devices.find(x => x.execute?.find(y => y.name == cmdName));
 
-        deviceName = args.device;
-
-      }
-
-    } else {
-      platforms.push(...FilteredPlatforms);
+    if (!device) {
+      throw "cmd is not valid for this platform";
     }
-    return { platforms, deviceName };
+
+    const executeConfig = device.execute!.find(x => x.name === cmdName)!;// platform.devices.filter(x => x.execute).map(x => x.execute!).flat(1).find(x => x.name)!;
+
+    return { platform, device, executeConfig };
   }
 
   async run() {
-    const { args: { platform, cmd }, flags } = this.parse(Execute);
+    const { args } = this.parse(Execute);
+    
+    const { platform, device, executeConfig } = this.parseArgs(args); // Device is needed for eval function
 
-    const cmd_string = "echo hello";
 
-    execSync(cmd_string, { stdio: 'inherit' })
-    // const toto = spawn(cmd_array.shift()!, cmd_array, { stdio: [process.stdin, process.stdout, process.stderr] });
+    const platformName = platform.name.replace(actionExpression, (_, group1) => eval(group1))
+    const name = executeConfig.name.replace(actionExpression, (_, group1) => eval(group1));
+    const cmd = executeConfig.cmd.replace(actionExpression, (_, group1) => eval(group1));
 
+    console.log(`Starting cmd ${name} on ${platformName}`);
+
+    try {
+      execSync(cmd, { stdio: 'inherit' })
+    } catch (error) {
+      // No need to print error message
+    }
 
   }
 
