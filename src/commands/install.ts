9867a04aee7c21cae8c746243504b994cbb4aeb8
Martin Gauthier <gauthiermartin86@gmail.com>
2020-07-06 16:23:10 -0400

    Started working on install

--- src/commands/install.ts
+++ src/commands/install.ts
@@ -1,1 +1,29 @@
 import { Command, flags } from '@oclif/command'
+import * as Listr from 'listr'
+import { command } from 'execa'
+
+export default class Install extends Command {
+  static description = 'Install AUV environment on one of the following plateform (Local, AUV7 or AUV8)'
+
+  static examples = [
+    '$ sonia install',
+    '$ sonia install local',
+    '$ sonia install auv7',
+    '$ sonia install auv8',
+  ]
+
+  static flags = {
+    help: flags.help({ char: 'h' }),
+  }
+
+  static args = [
+    {
+      name: 'platform',
+      description: 'Platform to target',
+    },
+  ]
+
+  async run() {
+    return true
+  }
+}
